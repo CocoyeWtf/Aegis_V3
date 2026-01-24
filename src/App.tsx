4c034dfc0d1b11fdb5a1df9561a046cd6101904f
Dominic Verleyen <dverleyen@sites.mousquetaires.grp>
2026-01-24 14:14:59 +0100

    feat: init v3 project structure

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-screen bg-black flex items-center justify-center text-white font-mono text-xl">
+      AEGIS V3 - SYSTEM INITIALIZED
+    </div>
+  );
+}
+
+export default App;
